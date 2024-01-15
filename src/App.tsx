1f33ff89e0947df445627c113458925a9406fe0d
Rodrigo Cândido <rodrigocan@gmail.com>
2024-01-15 19:46:55 -0300

    SEO

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { router } from "./routes";
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet />
+      <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
     </HelmetProvider>
   );
