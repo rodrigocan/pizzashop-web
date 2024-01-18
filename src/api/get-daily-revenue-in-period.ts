09c5189a1a735b72317c554585b2980aefd8daca
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2024-01-18 11:24:29 -0300

    Receita no período

---
+++ src/api/get-daily-revenue-in-period.ts
@@ -0,0 +1,28 @@
+import { api } from "@/lib/axios";
+
+export interface GetDailyRevenueInPeriod {
+  from?: Date;
+  to?: Date;
+}
+
+export type GetDailyRevenueInPeriodResponse = {
+  date: string;
+  receipt: number;
+}[];
+
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDailyRevenueInPeriod) {
+  const response = await api.get<GetDailyRevenueInPeriodResponse>(
+    "/metrics/daily-receipt-in-period",
+    {
+      params: {
+        from,
+        to,
+      },
+    },
+  );
+
+  return response.data;
+}
